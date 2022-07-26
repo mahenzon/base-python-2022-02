949be227f7a3674bcc2b7787a97e5bfe849615d8
Suren Khorenyan <surenkhorenyan@gmail.com>
2022-07-26 20:42:01 +0300

    create tests

---
+++ testing/conftest.py
@@ -0,0 +1,13 @@
+from flask.testing import FlaskClient
+
+from pytest import fixture
+
+from main import app
+
+
+@fixture
+def client() -> FlaskClient:
+    app.config.update(SERVER_NAME="server.org")
+    with app.test_client() as client:  # type: FlaskClient
+        with app.app_context():
+            yield client
